cc8c341d709d3cffc95a2b8928726d6527e95bfb
Emmanuel <ebube.okeke@hotmail.com>
2019-08-07 06:49:14 +0100

    Implemented authentication with JWT

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// JWT authentication
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 });
